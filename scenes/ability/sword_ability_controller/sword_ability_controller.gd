e8959b302a16cca73e0da43f33193000155b455e
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2024-05-28 00:37:19 +0900

    ~11 Destroying Enemies

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -30,3 +30,8 @@ func on_timer_timeout():
 	var sword_instance = sword_ability.instantiate() as Node2D
 	player.get_parent().add_child(sword_instance)
 	sword_instance.global_position = enemies[0].global_position
+	sword_instance.global_position += Vector2.RIGHT.rotated(randf_range(0, TAU)) * 4  #살짝 랜덤한 주변으로. (TAU = 2PI = 360 degree)
+
+	# 적 방향으로
+	var enemy_direction = enemies[0].global_position - sword_instance.global_position
+	sword_instance.rotation = enemy_direction.angle()
