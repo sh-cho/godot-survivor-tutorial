839c56e75143a014264966b3fabdd850fad306be
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2024-05-28 23:48:56 +0900

    ~16

--- scenes/game_object/game_camera/game_camera.gd
+++ scenes/game_object/game_camera/game_camera.gd
@@ -9,7 +9,7 @@ func _ready():
 
 func _process(delta):
 	acquire_target()
-	global_position = global_position.lerp(target_position, 1.0 - exp(-delta * 10))
+	global_position = global_position.lerp(target_position, 1.0 - exp(-delta * 20))
 
 
 func acquire_target():
---
+++ scenes/ui/arena_time_ui.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+@export var arena_time_manager: Node
+@onready var label = %Label
+
+func _process(delta):
+	if arena_time_manager == null:
+		return
+
+	var time_elapsed = arena_time_manager.get_time_elapsed()
+	label.text = format_seconds_to_string(time_elapsed)
+
+
+func format_seconds_to_string(seconds: float) -> String:
+	var minutes = floor(seconds / 60)
+	var remaining_seconds = floor(seconds - (minutes * 60))
+	return "%d:%02d" % [minutes, remaining_seconds]
