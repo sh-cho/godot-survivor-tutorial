70d4dbbd769ac2875e0e2c99ad25f189305925dd
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2024-05-29 22:42:11 +0900

    ~18 health, vial drop

---
+++ scenes/component/health_component.gd
@@ -0,0 +1,23 @@
+extends Node
+class_name HealthComponent
+
+signal died
+
+@export var max_health: float = 10
+var current_health: float
+
+
+func _ready():
+	current_health = max_health
+
+
+func damage(damage_amount: float):
+	# clamping
+	current_health = max(current_health - damage_amount, 0)
+	Callable(check_death).call_deferred()
+
+
+func check_death():
+	if current_health == 0:
+		died.emit()
+		owner.queue_free()
