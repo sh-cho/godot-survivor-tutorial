51944f6d9e648433efa9599777d6cca7a9167d5b
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2024-05-30 23:16:50 +0900

    ~21 custom resource

--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -2,6 +2,7 @@ extends Node
 class_name ExperienceManager
 
 signal experience_updated(current_experience: float, target_experience: float)
+signal level_up(new_level: int)
 
 const TARGET_EXPERIENCE_GROWTH = 5;
 
@@ -21,6 +22,7 @@ func increment_experience(number: float):
 		current_experience = 0
 		target_experience += TARGET_EXPERIENCE_GROWTH
 		experience_updated.emit(current_experience, target_experience)
+		level_up.emit(current_level)
 
 
 func on_experience_vial_collected(number: float):
