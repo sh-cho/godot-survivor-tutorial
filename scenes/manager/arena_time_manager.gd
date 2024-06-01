39354d53a58afceead01da25b74cf9546892f141
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2024-06-01 18:20:42 +0900

    ~30 increase difficulty over time

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,12 +1,27 @@
 extends Node
+class_name ArenaTimeManager
+
+signal arena_difficulty_increased(arena_difficulty: int)
+
+const DIFFICULTY_INTERVAL := 5
 
 @export var end_screen_scene: PackedScene
 
 @onready var timer = $Timer
 
+var arena_difficulty = 0
+
 
 func _ready():
 	timer.timeout.connect(on_timer_timeout)
+
+
+func _process(delta):
+	var next_time_target = timer.wait_time - ((arena_difficulty + 1) * DIFFICULTY_INTERVAL)
+	if timer.time_left <= next_time_target:
+		arena_difficulty += 1
+		arena_difficulty_increased.emit(arena_difficulty)
+		#print("difficulty: %d, time_left: %f, next_time_target: %f" % [arena_difficulty, timer.time_left, next_time_target])
 
 
 func get_time_elapsed():
@@ -16,3 +31,4 @@ func get_time_elapsed():
 func on_timer_timeout():
 	var end_screen_instance = end_screen_scene.instantiate()
 	add_child(end_screen_instance)
+	end_screen_instance.set_defeat()
--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -4,12 +4,22 @@ extends Node
 const SPAWN_RADIUS = 375
 
 @export var basic_enemy_scene: PackedScene
+@export var arena_time_manager: ArenaTimeManager
+
+@onready var timer = $Timer
+
+var base_spawn_time = 0  # sec
+
 
 func _ready():
-	$Timer.timeout.connect(on_timer_timeout)
+	base_spawn_time = timer.wait_time
+	timer.timeout.connect(on_timer_timeout)
+	arena_time_manager.arena_difficulty.connect(on_arena_difficulty_increased)
 
 
 func on_timer_timeout():
+	timer.start()
+	
 	# Spawn outside of the view
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
@@ -23,3 +33,10 @@ func on_timer_timeout():
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	entities_layer.add_child(enemy)
 	enemy.global_position = spawn_position
+
+
+func on_arena_difficulty_increased(arena_difficulty: int):
+	var time_off = (0.1 / 12) * arena_difficulty
+	time_off = min(time_off, 0.7)
+	timer.wait_time = base_spawn_time - time_off
+	
