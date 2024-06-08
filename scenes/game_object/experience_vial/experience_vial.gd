09cf6c15d22d89af247913115d654aa81d11dc8a
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2024-06-08 22:46:54 +0900

    ~57 sfx vial, player hit

--- scenes/game_object/experience_vial/experience_vial.gd
+++ scenes/game_object/experience_vial/experience_vial.gd
@@ -43,3 +43,5 @@ func on_area_entered(other_area: Area2D):
 	tween.tween_property(sprite, "scale", Vector2.ZERO, 0.05).set_delay(0.45)
 	tween.chain()
 	tween.tween_callback(collect)
+	
+	$RandomAudioStreamPlayer2DComponent.play_random()
