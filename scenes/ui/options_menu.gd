d3b8d74b086395b2eab20a3ed0af54d02ef16f76
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2024-06-15 14:32:52 +0900

    ~64 main, option menu

---
+++ scenes/ui/options_menu.gd
@@ -0,0 +1,64 @@
+extends CanvasLayer
+class_name OptionsMenu
+
+signal back_pressed
+
+@onready var window_button: Button = %WindowButton
+@onready var music_slider: Slider = %MusicSlider
+@onready var sfx_slider: Slider = %SfxSlider
+@onready var back_button: Button = %BackButton
+
+
+func _ready():
+	back_button.pressed.connect(on_back_pressed)
+	window_button.pressed.connect(on_window_button_pressed)
+	music_slider.value_changed.connect(on_audio_slider_changed.bind("music"))
+	sfx_slider.value_changed.connect(on_audio_slider_changed.bind("sfx"))
+	update_display()
+
+
+func update_display():
+	match DisplayServer.window_get_mode():
+		DisplayServer.WINDOW_MODE_FULLSCREEN:
+			window_button.text = "Fullscreen"
+		_:
+			window_button.text = "Windowed"
+	music_slider.value = get_bus_volume_percent("music")
+	sfx_slider.value = get_bus_volume_percent("sfx")
+
+
+func get_bus_volume_percent(bus_name: String):
+	var bus_index = AudioServer.get_bus_index(bus_name)
+	if bus_index == -1:
+		# TODO: assert or warn
+		return 0
+	var volume_db = AudioServer.get_bus_volume_db(bus_index)
+	return db_to_linear(volume_db)
+
+
+func set_bus_volume_percent(bus_name: String, percent: float) -> void:
+	var bus_index = AudioServer.get_bus_index(bus_name)
+	if bus_index == -1:
+		# TODO: assert or warn
+		return
+	var volume_db = linear_to_db(percent)
+	AudioServer.set_bus_volume_db(bus_index, volume_db)
+
+
+func on_window_button_pressed():
+	var mode := DisplayServer.window_get_mode()
+	if mode != DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+	update_display()
+
+
+func on_audio_slider_changed(value: float, bus_name: String):
+	set_bus_volume_percent(bus_name, value)
+
+
+
+func on_back_pressed():
+	back_pressed.emit()
