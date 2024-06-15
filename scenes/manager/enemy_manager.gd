63d6c8955946967e4899a2e2cd49d95fbdf208f8
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2024-06-16 02:58:42 +0900

    Build other os

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -30,9 +30,10 @@ func get_spawn_position() -> Vector2:
 	var random_direction := Vector2.RIGHT.rotated(randf_range(0, TAU))
 	for i in 4:
 		spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+		var additional_check_offset = random_direction * 20  # prevent stuck in a wall
 
 		# raycast check
-		var query_parameters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1 << 0)
+		var query_parameters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position + additional_check_offset, 1 << 0)
 		var result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_parameters)
 
 		if result.is_empty():
--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -42,5 +42,5 @@ func on_continue_button_pressed():
 
 func on_quit_button_pressed():
 	ScreenTransition.transition_to_scene("res://scenes/ui/main_menu.tscn")
-	await ScreenTransition.transitioned_halfway
 	get_tree().paused = false
+	await ScreenTransition.transitioned_halfway
