fd1b2efea61c4c9c76662e45d328b5132a909a8a
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2024-06-15 15:16:09 +0900

    ~65 pause menu

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -3,9 +3,18 @@ extends Node
 
 @export var end_screen_scene: PackedScene
 
+var paused_menu_scene = preload("res://scenes/ui/pause_menu.tscn")
+
 
 func _ready():
 	%Player.health_component.died.connect(on_player_died)
+
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		add_child(paused_menu_scene.instantiate())
+		get_tree().root.set_input_as_handled()
 
 
 func on_player_died():
---
+++ scenes/ui/pause_menu.gd
@@ -0,0 +1,66 @@
+extends CanvasLayer
+class_name PauseMenu
+
+@onready var panel_container = %PanelContainer
+
+var options_scene = preload("res://scenes/ui/options_menu.tscn")
+var is_closing := false
+
+
+func _ready():
+	get_tree().paused = true
+	panel_container.pivot_offset = panel_container.size / 2
+	
+	%ResumeButton.pressed.connect(on_resume_pressed)
+	%OptionsButton.pressed.connect(on_options_pressed)
+	%QuitButton.pressed.connect(on_quit_pressed)
+	
+	$AnimationPlayer.play("default")
+	
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, 0.3) \
+		.set_ease(Tween.EASE_OUT) \
+		.set_trans(Tween.TRANS_BACK)
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		close()
+		get_tree().root.set_input_as_handled()
+
+
+func close():
+	if is_closing:
+		return
+	is_closing = true
+	
+	$AnimationPlayer.play_backwards("default")
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ONE, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0.3) \
+		.set_ease(Tween.EASE_IN) \
+		.set_trans(Tween.TRANS_BACK)
+	await tween.finished
+	
+	get_tree().paused = false
+	queue_free()
+
+
+func on_resume_pressed():
+	close()
+
+
+func on_options_pressed():
+	var options_instance = options_scene.instantiate() as OptionsMenu
+	add_child(options_instance)
+	options_instance.back_pressed.connect(on_options_closed.bind(options_instance))
+
+
+func on_options_closed(options_instance: OptionsMenu):
+	options_instance.queue_free()
+
+
+func on_quit_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
